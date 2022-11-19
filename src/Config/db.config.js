5721cb64d33dbc851e6884d1b494e9677423b840
krupal.dds <krupal.dds@gmail.com>
2022-11-19 10:26:38 +0530

    change in db URL

--- src/Config/db.config.js
+++ src/Config/db.config.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 const dotenv = require('dotenv').config();
 const pool = new Pool({
   user: process.env.USER,
-  host: "localhost",
+  host: "https://enrique-backend-api.herokuapp.com",
   database: process.env.DB ,
   password: process.env.PASSWORD,
   port: 5432,
