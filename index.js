c0041b80d2278ac5e3b09648878e08fd4a3dca1c
krupal.dds <krupal.dds@gmail.com>
2022-11-19 12:23:39 +0530

    solve database error

--- index.js
+++ index.js
@@ -17,3 +17,5 @@ require('./src/Route/Auth')(app);
 app.listen(port, () => {
   console.log(`Connect To Server ${port}`);
 });
+
+    
--- src/Config/db.config.js
+++ src/Config/db.config.js
@@ -2,10 +2,27 @@ const Pool = require("pg").Pool;
 const dotenv = require('dotenv').config();
 const pool = new Pool({
   user: process.env.USER,
-  host: "https://enrique-backend-api.herokuapp.com",
+  host: process.env.HOST,
   database: process.env.DB ,
   password: process.env.PASSWORD,
   port: 5432,
+  ssl: {
+    rejectUnauthorized: false,
+}
 })
+pool.connect().then(()=>{
+  console.log("Connect to DB!!!");
+}).catch((err)=>{
+  console.log(err.message,'...err');
+})
+// const Pool = require("pg").Pool;
+// const dotenv = require('dotenv').config();
+// const pool = new Pool({
+//   user: process.env.USER,
+//   host: "https://enrique-backend-api.herokuapp.com",
+//   database: process.env.DB ,
+//   password: process.env.PASSWORD,
+//   port: 5432,
+// })
 
 module.exports = pool
